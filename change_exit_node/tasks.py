e577bca4e39a14338da464c75a6e91fd1f0ee91c
gillesxr <gillesxr@tutanota.com>
2024-10-09 15:44:46 +0200

    Add change_node function in tasks.py

--- change_exit_node/tasks.py
+++ change_exit_node/tasks.py
@@ -15,6 +15,10 @@ import os
 import re
 import typing
 
+# See 'https://b3rn3d.herokuapp.com/blog/2014/03/05/tor-country-codes' for full list.
+countries_nodes = {'belgium': 'be', 'france': 'fr', 'italy': 'it', 'japan': 'jp',
+                   'spain': 'sp', 'switzerland': 'ch', 'usa': 'us'}
+
 def get_node_from_country(country: str) -> str:
     """
        Convert the country name to country code.
@@ -26,16 +30,48 @@ def get_node_from_country(country: str) -> str:
        :rtype: str
     """
     try:
-        return {'belgium': 'be',
-                'france': 'fr',
-                'italy': 'it',
-                'japan': 'jp',
-                'spain': 'sp',
-                'switzerland': 'ch',
-                'usa': 'us'}[country.lower()]
+        return countries_nodes[country.lower()]
     except KeyError:
         print(f'{country} not listed or unknown.')
         return 'error'
+
+def change_node(torrc: typing.TextIO, country_code: str) -> bool:
+    """
+       Change the first exit nodes (country code) in the torrc file.
+
+       :param torrc: The Tor browser configuration filename.
+       :type torrc: TextIO
+       :param country_code: The country code of the exit node.
+       :type country_code: str
+
+       :return: True if succeed else False
+       :rtype: bool
+    """
+    if country_code in countries_nodes.values():
+        line_number = 0
+        line_to_modify = ''
+        exit_nodes = None
+        offset = 0
+        try:
+            with open(torrc, 'r+') as fdesc:
+                for nb_line, line in enumerate(fdesc, 1):
+                    if 'ExitNodes' in line:
+                        line_number = nb_line
+                        line_to_modify = line
+                        exit_nodes = re.findall(r'{(\w+)}', line)
+                        break
+                    offset += len(line)
+                if line_number != 0:
+                    line_to_modify = line_to_modify.replace(f'{{{exit_nodes[0]}}}', f'{{{country_code}}}')
+                    fdesc.seek(offset)
+                    fdesc.write(line_to_modify)
+                    fdesc.flush()
+        except FileNotFoundError:
+            print('Please give a valid torrc file!')
+            return False
+        return True
+    else:
+        return False
 
 def get_current_node(torrc: typing.TextIO) -> str:
     """
--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -1,6 +1,8 @@
 from change_exit_node.tasks import *
+import linecache
 import os
 import pytest
+import shutil
 
 @pytest.fixture
 def setup_dir():
@@ -8,6 +10,10 @@ def setup_dir():
     os.chdir('./tests')
     yield None
     os.chdir(current_dir)
+
+@pytest.fixture
+def setup_file(setup_dir):
+    shutil.copy('./torrc.fortest.orig', './torrc.fortest')
 
 def test_get_current_node_with_no_defined_node_name(setup_dir):
     assert get_current_node('empty_torrc') == ''
@@ -34,3 +40,13 @@ def test_get_node_from_country_with_known_countries():
 
 def test_get_node_from_country_with_unknown_country():
     assert get_node_from_country('Germany') == 'error'
+
+def test_change_node_returns_true_if_succeed_to_change_exit_node(setup_file):
+    assert change_node('torrc.fortest', 'be') == True
+    assert linecache.getline('torrc.fortest', 26) == 'ExitNodes {be}, {us}, {fr}, {ch}\n'
+
+def test_change_node_returns_false_if_fail_to_change_exit_node(setup_file):
+    assert change_node('torrc.fortest', 'cc') == False
+
+def test_change_node_with_not_existing_file_returns_false():
+    assert change_node('not_a_file', 'be') == False
